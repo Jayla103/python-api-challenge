ddda517812fc9c4ff1e6c202fae8e6f4dcd00888
Jayla103 <jaylagary45@gmail.com>
2025-04-20 12:32:30 -0500

    Update api_keys.py

--- Module 6/Starter_Code/api_keys.py
+++ Module 6/Starter_Code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "dbea2f2271b7add3698326d3ed5a1a91"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "4a5189f716904de8ad6824e8c87b1a19"
